5afebd1bcd5b1242141125fbda12197625c45f64
LAPTOP-IQBRE5L0\hyunm <hyunmin_29@naver.com>
2018-06-01 15:16:23 +0900

    Find Password Activity 추가

---
+++ app/src/main/java/org/androidtown/newjjjproject/FindPwActivity.java
@@ -0,0 +1,13 @@
+package org.androidtown.newjjjproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FindPwActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_pw);
+    }
+}
