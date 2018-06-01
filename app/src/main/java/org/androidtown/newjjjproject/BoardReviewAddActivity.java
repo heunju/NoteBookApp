1e72cf001d5a1f29d44b04e7441fbfc64e54dadf
jikur <jikur3667@naver.com>
2018-06-01 15:13:04 +0900

    BoardActivity RatingBar

--- app/src/main/java/org/androidtown/newjjjproject/BoardReviewAddActivity.java
+++ app/src/main/java/org/androidtown/newjjjproject/BoardReviewAddActivity.java
@@ -11,24 +11,39 @@ import android.widget.RatingBar;
 public class BoardReviewAddActivity extends AppCompatActivity {
 
     Button btn_writeReview;
-    EditText edit_wirteReview;
-    RatingBar ratingBar2;
+    EditText edit_writeReview;
+    RatingBar ratingBar_review;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_board_review_add);
 
-        initview();
+        initView();
+
+        ratingBar_review.setNumStars(5);
+        ratingBar_review.setStepSize((float)0.5);
+        ratingBar_review.setRating((float)2.5);
+        ratingBar_review.setIsIndicator(false);
+
+        ratingBar_review.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float v, boolean b) {
+
+
+            }
+        });
+
+
     }
 
-    public void initview()
+
+    public void initView()
     {
-        btn_writeReview = findViewById(R.id.btn_wirteReview);
-        edit_wirteReview = findViewById(R.id.edit_wirteReview);
-        ratingBar2 = findViewById(R.id.ratingBar2);
+        btn_writeReview = findViewById(R.id.btn_writeReview);
+        edit_writeReview = findViewById(R.id.edit_writeReview);
+        ratingBar_review = findViewById(R.id.ratingBar_review);
     }
-
 
 
     public void write(View view)
@@ -36,6 +51,9 @@ public class BoardReviewAddActivity extends AppCompatActivity {
         Intent intent = new Intent(this, BoardActivity.class);
         startActivity(intent);
     }
+
+
+
 
 
 
